8d735fead221e0985dcd18df8d21fc2b301e81a2
Brett Owers <owersbrett@gmail.com>
2019-12-05 12:43:45 -0700

    not 42 and not 84

--- answer.js
+++ answer.js
@@ -1,1 +1,9 @@
 // *** YOUR ANSWER BELOW ***
+function divisible(){
+    for (let i = 1; i <= 100; i++){
+        if (((i % 6 === 0) && (i % 7 !== 0)) || ((i % 6 !== 0) && (i % 7 === 0))){
+                console.log(i);
+        }
+    }
+}
+divisible();
